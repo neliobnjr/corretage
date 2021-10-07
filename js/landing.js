e86ebeb55ff6befc4f931861dfecbe1f0adf55a8
Nelio Batista <nelio.batistadonasci@ndus.edu>
2021-10-07 20:52:38 -0300

    initial commits

---
+++ js/landing.js
@@ -0,0 +1,12 @@
+HTML CSS JSResult Skip Results Iframe
+ const menuToggle = document.querySelector('.toggle');
+      const showcase = document.querySelector('.showcase');
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        showcase.classList.toggle('active');
+      })
+
+
+
+Resources1× 0.5× 0.25×Rerun
