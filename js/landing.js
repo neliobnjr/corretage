5bd019e6cf48f0ab59fefef2bb282ae9137a09b5
Nelio Batista <nelio.batistadonasci@ndus.edu>
2021-10-10 19:02:34 -0300

    Update landing.js

--- js/landing.js
+++ js/landing.js
@@ -1,12 +1,7 @@
-HTML CSS JSResult Skip Results Iframe
- const menuToggle = document.querySelector('.toggle');
+const menuToggle = document.querySelector('.toggle');
       const showcase = document.querySelector('.showcase');
 
       menuToggle.addEventListener('click', () => {
         menuToggle.classList.toggle('active');
         showcase.classList.toggle('active');
       })
-
-
-
-Resources1× 0.5× 0.25×Rerun
